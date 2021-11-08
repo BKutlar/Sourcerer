440fe54bc70833ca469c27eaa7d68eca0cb2351b
BKutlar <brayankutlar94@gmail.com>
2021-11-08 15:07:50 +0100

    test

---
+++ my-app/react-apollo/src/components/app/app.component.tsx
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import Header from '../header/header.component';
+import './app.component.css'; 
+import '../../common/styles'; 
+import About from '../../pages/about/about.page';
+
+const App: React.FC = ()=> {
+    return (
+        
+        <Header /> 
+
+    ); 
+}
+
+export default App; 
---
+++ test/index.js
@@ -0,0 +1,40 @@
+/*import {
+    ApolloClient,
+    InMemoryCache,
+    gql
+  } from "@apollo/client";
+import { graphQLResultHasError } from "@apollo/client/utilities";
+  
+  const client = new ApolloClient({
+    uri: 'https://48p1r2roz4.sse.codesandbox.io',
+    cache: new InMemoryCache()
+  });
+  // const client = ...
+
+client
+.query({
+  query: gql`
+    query GetRates {
+      rates(currency: "USD") {
+        currency
+      }
+    }
+  `
+})*/
+//.then(result => console.log(result)); 
+
+
+
+/*const express = require ("express");
+const app = express(); 
+const graphql =  require("graphql");
+const {graphqlHTTP} = require("express-graphql");
+const RootQuery = "query"; 
+const Mutation = "mutation"; 
+const schema = new GraphQLSchema{(query: RootQuery , mutation: Mutation)};
+
+
+app.use ('/graphql', graphqlHTTP({
+  schema, 
+  graphql: true
+})) */
