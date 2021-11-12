40678a732f659686aeb985a3189c92e27f80db25
BKutlar <brayankutlar94@gmail.com>
2021-11-12 22:46:07 +0100

    sourcerer

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,3 +3,29 @@ import * as LibApollo from 'apollo-client'
 
 export const gql = libgql
 export const lib = LibApollo
+import { ApolloClient, createHttpLink, InMemoryCache, gql } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
+export async function getStaticProps() {
+  return {
+    props: {
+    }
+  }
+}
+
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
